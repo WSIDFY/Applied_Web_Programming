12e7a52f8f53b6f646329e2611057af1f5650bb5
K_blueprint <141166836+WSIDFY@users.noreply.github.com>
2025-03-09 23:58:43 +0900

    Add files via upload

---
+++ 1week/chapter_03/Library.jsx
@@ -0,0 +1,14 @@
+// React 모듈 가져오기
+import React from "react";
+import Book from "./Book";
+
+function Library(props) {
+    return (
+        <div>
+            <Book name="처음 만난 파이썬" numOfPage={300} />
+            <Book name="처음 만난 AWS" numOfPage={400} />
+            <Book name="처음 만난 리액트" numOfPage={500} />
+        </div>
+    );
+ }
+ export default Library;
