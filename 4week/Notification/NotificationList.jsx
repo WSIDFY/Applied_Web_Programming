d407984ed80969f4c84b5c5b3b4923fc92820be5
K_blueprint <141166836+WSIDFY@users.noreply.github.com>
2025-03-30 20:33:15 +0900

    Update NotificationList.jsx

--- 4week/Notification/NotificationList.jsx
+++ 4week/Notification/NotificationList.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Notification from "./Notification";
+
+// 해당 배열에 정의된 알림들이 순차적으로 화면에 출력
 const reservedNotifications = [
     {
         id: 1,
@@ -14,36 +16,47 @@ const reservedNotifications = [
         message: "이제 곧 미팅이 시작됩니다.",
     },
 ];
+
+// 타이머 변수를 전역 변수로 선언
+// setInterval 및 clearInterval에서 사용
 var timer;
 class NotificationList extends React.Component {
     constructor(props) {
         super(props);
+        // 초기 state는 notification 배열로 지정(처음에는 비어있음)
         this.state = {
             notifications: [],
         };
     }
     componentDidMount() {
         const { notifications } = this.state;
+        // 1초마다 실행되는 타이머 설
         timer = setInterval(() => {
+            // 예약된 알림의 개수보다 현재 notifications의 개수가 적으면 실행
             if (notifications.length < reservedNotifications.length) {
+                // 현재 notifications의 길이를 인덱스로 사용하여 예약된 알림을 추가
                 const index = notifications.length;
                 notifications.push(reservedNotifications[index]);
                 this.setState({
                     notifications: notifications,
                 });
             } else {
+                // 모든 알림을 다 표시한 경우, notifications 배열을 초기화
                 this.setState({
                     notifications: [],
                 });
+                // 타이머 정지
                 clearInterval(timer);
             }
         }, 1000);
     }
+    // 컴포넌트가 언마운트되기 전에 호출되는 라이프사이클 메서드
     componentWillUnmount() {
         if (timer) {
             clearInterval(timer);
         }
     }
+    // render 메서드: 화면에 렌더링할 UI 정의
     render() {
         return (
             <div>
