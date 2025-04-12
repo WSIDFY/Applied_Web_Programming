443fcdf671b04ce19348cef579245e0891dcfad7
K_blueprint <141166836+WSIDFY@users.noreply.github.com>
2025-04-13 00:18:32 +0900

    Update index.js

--- 6week/index.js
+++ 6week/index.js
@@ -5,27 +5,3 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
-
-
-
-
-
-/*
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App'; //myapp으로 변경해야함함
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( // 실질적으로 화면에 뿌려지는 내용
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-*/
